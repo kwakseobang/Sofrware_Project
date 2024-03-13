d5cf804068c9553d7cdb745f2861b218dac05810
Kwakmunsu <iii6602@naver.com>
2024-03-14 02:53:28 +0900

    first commit

---
+++ SoftWare_Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Software_Project
+//
+//  Created by 곽서방 on 3/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SoftWare_Project/Software_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Software_ProjectApp.swift
+//  Software_Project
+//
+//  Created by 곽서방 on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Software_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
